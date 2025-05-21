50f8867aa1e0ddfbbf5c05a199fccc6395c95fdd
DuongBachSang <duongsang.28102006@gmail.com>
2025-05-21 22:15:51 +0700

    sang

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-public static void main (String [] args){
-    System.out.println("Hello Vitamin Code 03");
-    System.out.println("Hello Admin");
+    public static void main (String [] args){
+        System.out.println("Hello Vitamin Code 03");
+        System.out.println("Hello Admin");
     }
 }
